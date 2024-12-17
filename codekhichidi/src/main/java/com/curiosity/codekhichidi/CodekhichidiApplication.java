9ebeda5948fbaa07bfe89c80832383e313b99e59
aman-g0yaa <amangoyal2339@gmail.com>
2024-12-18 03:11:03 +0530

    Intial Commit For Spring

---
+++ codekhichidi/src/main/java/com/curiosity/codekhichidi/CodekhichidiApplication.java
@@ -0,0 +1,13 @@
+package com.curiosity.codekhichidi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodekhichidiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodekhichidiApplication.class, args);
+	}
+
+}
---
+++ codekhichidi/src/test/java/com/curiosity/codekhichidi/CodekhichidiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.curiosity.codekhichidi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodekhichidiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
