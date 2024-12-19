2a37b825b3c2c7eaaba31aec307b40451d3469af
aman-g0yaa <amangoyal2339@gmail.com>
2024-12-19 23:33:12 +0530

    Added new HTTP Reqeust client via webflux to hit remote rest api

--- codekhichidi/src/main/java/com/curiosity/codekhichidi/Controller/HelloController.java
+++ codekhichidi/src/main/java/com/curiosity/codekhichidi/Controller/HelloController.java
@@ -1,17 +1,28 @@
 package com.curiosity.codekhichidi.Controller;
 
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.GetMapping;
+import reactor.core.publisher.Mono;
+import org.springframework.web.reactive.function.client.WebClient;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.curiosity.codekhichidi.model;
 
 @RestController
 public class HelloController {
 
-    @GetMapping("/hello")
-    public String getHello() {
-        return "<b> Hello World </b>";
-    }
-    
+    //https://jsonplaceholder.typicode.com/todos/1
 
+    @GetMapping("/fetch")
+    public String getFetch() {
+                
+        String url = "https://jsonplaceholder.typicode.com";
 
+        WebClient webClient = WebClient.create(url);
+
+        Mono<model> res = webClient.get().uri("/todos/1").retrieve().bodyToMono(model.class);
+        
+        return res.block().toString();
+        }
 }
