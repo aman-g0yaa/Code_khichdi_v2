f6c7c9dd2e73404ada5fb7beee0613a919092e59
aman-g0yaa <amangoyal2339@gmail.com>
2024-12-19 23:45:50 +0530

    updated previous http request with path variables

--- codekhichidi/src/main/java/com/curiosity/codekhichidi/Controller/HelloController.java
+++ codekhichidi/src/main/java/com/curiosity/codekhichidi/Controller/HelloController.java
@@ -5,24 +5,24 @@ import reactor.core.publisher.Mono;
 import org.springframework.web.reactive.function.client.WebClient;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import com.curiosity.codekhichidi.model;
 
 @RestController
 public class HelloController {
 
-    //https://jsonplaceholder.typicode.com/todos/1
+    // https://jsonplaceholder.typicode.com/todos/1
 
-    @GetMapping("/fetch")
-    public String getFetch() {
-                
+    @GetMapping("/fetch/{id}")
+    public String getFetch(@PathVariable String id) {
+
         String url = "https://jsonplaceholder.typicode.com";
 
         WebClient webClient = WebClient.create(url);
 
-        Mono<model> res = webClient.get().uri("/todos/1").retrieve().bodyToMono(model.class);
-        
+        Mono<model> res = webClient.get().uri("/todos/" + id).retrieve().bodyToMono(model.class);
+
         return res.block().toString();
-        }
+    }
 }
